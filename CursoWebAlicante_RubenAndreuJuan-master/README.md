05895cf4d0040e4ff77fe6641952c1590fe2e9a6
Ruben Andreu Juan <40172854+rubenaj@users.noreply.github.com>
2019-04-05 11:55:14 +0200

    Add files via upload
    trabajo final mi propia pagina web

---
+++ CursoWebAlicante_RubenAndreuJuan-master/README.md
@@ -0,0 +1,18 @@
+# CursoWebAlicante_RubenAndreuJuan
+
+Trabajo Realizado para el curso de la Web BOOTCAMP DE DESARROLLO WEB
+
+el trabajo incluye 
+
+- HTML5
+- CSS 3
+- JAVASCRIPT
+- PHP (este formulario lo utilice en un trabajo del FP solamente he conseguido adaptarlo al proyecto).
+- La libreria photoswipe background es una galeria que cogi de un github la vi muy adecuada para este proyecto y solamente la he introducido en el proyecto no he cambiado ningun texto de ipsulum, ya que es una galeria que m,e gusto para el trabajo
+- las barritas de habilidades las hice con html5, bootstrap y jquery.
+-Ademas incorpore algun apartado de Sass por el proyecto como puede ser el formulario.
+- El resto ha sido jugar con html y bootstrap 
+
+Casi Todos los comentarios del programa viene en Ingles para un mejor entendimiento del codigo
+
+RUBEN ANDREU JUAN
---
+++ CursoWebAlicante_RubenAndreuJuan-master/inc/sendEmail.php
@@ -0,0 +1,68 @@
+﻿<?php
+
+$siteOwnersEmail = 'rubenaj88@gmail.com';
+
+
+if($_POST) {
+
+    $name = trim(stripslashes($_POST['contactName']));
+    $email = trim(stripslashes($_POST['contactEmail']));
+    $subject = trim(stripslashes($_POST['contactSubject']));
+    $contact_message = trim(stripslashes($_POST['contactMessage']));
+
+    // Check Name
+    if (strlen($name) < 2) {
+        $error['name'] = "Por favor, escriba su nombre.";
+    }
+    // Check Email
+    if (!preg_match('/^[a-z0-9&\'\.\-_\+]+@[a-z0-9\-]+\.([a-z0-9\-]+\.)*+[a-z]{2}/is', $email)) {
+        $error['email'] = "Por favor, introduce una dirección de correo electrónico válida.";
+    }
+    // Check Message
+    if (strlen($contact_message) < 15) {
+        $error['message'] = "Por favor ingrese su mensaje. Debe tener al menos 15 caracteres.";
+    }
+    // Subject
+    if ($subject == '') { $subject = "Formulario de contacto de envío"; }
+
+
+    // Set Message
+    $message .= "Email de: " . $name . "<br />";
+    $message .= "Dirección de correo electrónico: " . $email . "<br />";
+    $message .= "Mensaje: <br />";
+    $message .= $contact_message;
+    $message .= "<br /> ----- <br /> Este correo electrónico fue enviado desde el formulario de contacto de su sitio.. <br />";
+
+    // Set From: header
+    $from =  $name . " <" . $email . ">";
+
+    // Email Headers
+    $headers = "De: " . $from . "\r\n";
+    $headers .= "responder a: ". $email . "\r\n";
+    $headers .= "MIME-Version: 1.0\r\n";
+    $headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
+
+
+    if (!$error) {
+
+        ini_set("sendmail_from", $siteOwnersEmail); // for windows server
+        $mail = mail($siteOwnersEmail, $subject, $message, $headers);
+
+        if ($mail) { echo "OK"; }
+        else { echo "Algo salió mal. Inténtalo de nuevo."; }
+        
+    } # end if - no validation error
+
+    else {
+
+        $response = (isset($error['name'])) ? $error['name'] . "<br /> \n" : null;
+        $response .= (isset($error['email'])) ? $error['email'] . "<br /> \n" : null;
+        $response .= (isset($error['message'])) ? $error['message'] . "<br />" : null;
+        
+        echo $response;
+
+    } # end if - there was a validation error
+
+}
+
+?>
